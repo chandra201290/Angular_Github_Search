c7c1ffa43935e2e0f228524450d55c04c56b9ab8
chandra201290 <chandra201290@gmail.com>
2018-05-15 22:45:39 +0530

    server.js

--- server.js
+++ server.js
@@ -8,14 +8,9 @@ const app = express();
 app.use(express.static(__dirname + '/dist'));
 
 app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname + '/dist/'+ 'index.html'));
+
+  res.sendFile(path.join(__dirname+'/dist/index.html'));
 });
-/*app.get('/!*', function (req, res) {
-  const index = path.join(__dirname, 'dist', 'index.html');
-  res.sendFile(index);
-});*/
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-});
+app.listen(process.env.PORT || 8080);
